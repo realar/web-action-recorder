e1aeb47773f46b96271b601932349c9ea12d70e8
Lev Korotich <51894137+realar@users.noreply.github.com>
2024-11-06 16:03:36 +0700

    Update background.js

--- background.js
+++ background.js
@@ -1,20 +1,29 @@
 // background.js
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === "record_click") {
-        // Получение текущего времени
+    if (request.type === "record_click_request") {
+        const description = request.description;
         const timestamp = new Date().toLocaleString();
 
-        // Сохранение действия в хранилище
-        chrome.storage.local.get({steps: []}, (data) => {
-            const steps = data.steps;
-            steps.push({
-                timestamp: timestamp,
-                description: request.description,
-                screenshot: request.screenshot
-            });
-            chrome.storage.local.set({steps: steps}, () => {
-                sendResponse({status: "success"});
+        // Создаем скриншот видимой части вкладки
+        chrome.tabs.captureVisibleTab(sender.tab.windowId, {format: "png"}, (dataUrl) => {
+            if (chrome.runtime.lastError) {
+                console.error("Ошибка при создании скриншота:", chrome.runtime.lastError);
+                sendResponse({status: "failure"});
+                return;
+            }
+
+            // Сохранение действия в хранилище
+            chrome.storage.local.get({steps: []}, (data) => {
+                const steps = data.steps;
+                steps.push({
+                    timestamp: timestamp,
+                    description: description,
+                    screenshot: dataUrl
+                });
+                chrome.storage.local.set({steps: steps}, () => {
+                    sendResponse({status: "success"});
+                });
             });
         });
 
